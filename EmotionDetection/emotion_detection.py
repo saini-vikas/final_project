e2a8cce430bd11f3e2ab81b6834ab07dd413e598
Vikas Saini <vikassaini883@gmail.com>
2026-05-22 19:10:41 -0400

    added init.py

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,23 @@
+
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = myobj, headers=headers)
+    formatted_response = json.loads(response.text)
+    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    
+    result = {
+        'anger': emotion_scores.get('anger', 0),
+        'disgust': emotion_scores.get('disgust', 0),
+        'fear': emotion_scores.get('fear', 0),
+        'joy': emotion_scores.get('joy', 0),
+        'sadness': emotion_scores.get('sadness', 0),
+        'dominant_emotion': dominant_emotion
+    }
+    
+    return result
